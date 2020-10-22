8943fc6540c8b25713a8ab14f3e22f9ca8e10572
vlaksi <daksrapsody@gmail.com>
2020-10-22 15:04:58 +0200

    feat: Added CodeModel for ContentRepository

---
+++ SmartTutor/CodeModel/EducationContent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace SmartTutor.CodeModel
+{
+    public class EducationContent
+    {
+        public int Quaility { get; set; }
+        public EducationType Type { get; set; }
+    }
+}
---
+++ SmartTutor/CodeModel/EducationType.cs
@@ -0,0 +1,12 @@
+﻿namespace SmartTutor.CodeModel
+{
+    public class EducationType
+    {
+        // TODO: Change from string to some better model ?
+        public string CodeSnippet { get; set; }
+        public string LongText { get; set; }
+        public string ShortText { get; set; }
+        public string Video { get; set; }
+        public string Diagram { get; set; }
+    }
+}
