5286330aacb35bf2cbe3fb2a2d15cb5b1483685e
Pufke <pufke1998@gmail.com>
2020-10-29 13:47:24 +0100

    docs: DOI for ATFD

--- RepositoryCompiler/CodeModel/CaDETModel/Metrics/CaDETClassMetrics.cs
+++ RepositoryCompiler/CodeModel/CaDETModel/Metrics/CaDETClassMetrics.cs
@@ -7,6 +7,10 @@
         public int NMD { get; set; }
         public int NAD { get; set; }
         public int WMC { get; set; }
+        /// <summary>
+        /// ATFD: Access To Foreign Data
+        /// DOI: 10.1109/ESEM.2009.5314231
+        /// </summary>
         public int ATFD { get; set; }
     }
 }
