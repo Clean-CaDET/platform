200e52dbbeae7cbe47962b627345d8313e106d27
Pufke <pufke1998@gmail.com>
2020-11-05 15:29:28 +0100

    feat: initial commit for God Class rule engine

---
+++ SmellDetector/Detectors/RuleEngines/GodClassRuleEngine.cs
@@ -0,0 +1,13 @@
+﻿using SmellDetector.SmellModel;
+using SmellDetector.SmellModel.Reports;
+
+namespace SmellDetector.Detectors
+{
+    internal class GodClassRuleEngine : IDetector
+    {
+        public PartialSmellDetectionReport FindIssues(CaDETClassDTO caDetClassDto)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
