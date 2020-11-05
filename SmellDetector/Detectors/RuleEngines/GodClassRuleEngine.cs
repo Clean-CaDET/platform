f11b8e73eb10567519fd2fb279406a177da66447
Pufke <pufke1998@gmail.com>
2020-11-05 15:47:15 +0100

    feat: implementation god class rule engine

--- SmellDetector/Detectors/RuleEngines/GodClassRuleEngine.cs
+++ SmellDetector/Detectors/RuleEngines/GodClassRuleEngine.cs
@@ -1,5 +1,7 @@
-﻿using SmellDetector.SmellModel;
+﻿using SmellDetector.Controllers;
+using SmellDetector.SmellModel;
 using SmellDetector.SmellModel.Reports;
+using System;
 
 namespace SmellDetector.Detectors
 {
@@ -7,7 +9,31 @@ namespace SmellDetector.Detectors
     {
         public PartialSmellDetectionReport FindIssues(CaDETClassDTO caDetClassDto)
         {
-            throw new System.NotImplementedException();
+            PartialSmellDetectionReport partialReport = new PartialSmellDetectionReport();
+
+            foreach (var identifierAnalysis in caDetClassDto.CodeItemMetrics)
+            {
+
+                if (IsBadSmell(identifierAnalysis.Value))
+                {
+                    Issue newIssue = new Issue();
+                    newIssue.IssueType = SmellType.GOD_CLASS;
+                    newIssue.CodeItemId = identifierAnalysis.Key;
+                    partialReport.AddIssue(identifierAnalysis.Key, newIssue);
+                }
+
+            }
+
+            return partialReport;
+        }
+
+        private bool IsBadSmell(MetricsDTO metrics)
+        {
+            if (metrics.WMC > 47 && metrics.ATFD > 5 && metrics.TCC < 0.33) return true;
+            if (metrics.LOC > 100) return true;
+            if ((metrics.NMD + metrics.NAD) > 20) return true;
+
+            return false;
         }
     }
 }
