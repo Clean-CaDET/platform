9dc6feb12650dd9f8e71a70d293a7598303e44bd
Pufke <pufke1998@gmail.com>
2020-11-12 11:57:42 +0100

    feat: Initial commit SmartTutor Communication

---
+++ SmartTutor/Communication/SmartTutorMessageConsumer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartTutor.Communucation
+{
+    public class SmartTutorMessageConsumer
+    {
+    }
+}
