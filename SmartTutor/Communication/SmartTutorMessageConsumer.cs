80032ef24f49c67a8c0150cd128646ad65fc63f6
Pufke <pufke1998@gmail.com>
2020-11-12 12:25:17 +0100

    feat: Base class for reciving messages in Smart Tutor

--- SmartTutor/Communication/SmartTutorMessageConsumer.cs
+++ SmartTutor/Communication/SmartTutorMessageConsumer.cs
@@ -1,11 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
 
 namespace SmartTutor.Communucation
 {
+
     public class SmartTutorMessageConsumer
     {
+        public SmartTutorMessageConsumer()
+        {
+            var nodeName = "localhost";
+            var queueName = "IssueReports";
+            var exchangeName = "";
+
+            var connectionFactory = new ConnectionFactory() { HostName = nodeName };
+            using (var connection = connectionFactory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: queueName,
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+                channel.BasicConsume(queue: "hello",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
     }
 }
