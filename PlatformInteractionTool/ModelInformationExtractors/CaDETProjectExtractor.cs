1ec066cb4101f41630cd1689bfae3b3171c8c527
Nikola Luburic <luburic91@gmail.com>
2020-11-19 13:13:13 +0100

    Feat: Extracts project class and method names to file

---
+++ PlatformInteractionTool/ModelInformationExtractors/CaDETProjectExtractor.cs
@@ -0,0 +1,88 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+using RepositoryCompiler.CodeModel;
+using RepositoryCompiler.CodeModel.CaDETModel;
+using RepositoryCompiler.CodeModel.CaDETModel.CodeItems;
+
+namespace PlatformInteractionTool.ModelInformationExtractors
+{
+    class CaDETProjectExtractor
+    {
+        private readonly CaDETProject _project;
+        private readonly string _resultFolder;
+
+        public CaDETProjectExtractor(string projectPath) : this(projectPath, LanguageEnum.CSharp) {}
+
+        public CaDETProjectExtractor(string projectPath, LanguageEnum language)
+        {
+            _project = new CodeModelBuilder(language).ParseFiles(projectPath);
+            _resultFolder = projectPath + "extraction-results\\";
+        }
+
+        public void ExtractNamesToFile()
+        {
+            SaveClassNamesToFile();
+            SaveMethodAndConstructorNamesToFile();
+        }
+
+        private void SaveClassNamesToFile()
+        {
+            var sb = new StringBuilder();
+            foreach (var projectClass in _project.Classes)
+            {
+                sb.Append(projectClass.FullName).Append("\n");
+            }
+            WriteToFile(sb.ToString(), "classNames.txt");
+        }
+        private void SaveMethodAndConstructorNamesToFile()
+        {
+            var sb = new StringBuilder();
+            foreach (var projectClass in _project.Classes)
+            {
+                foreach (var member in projectClass.Members.Where(
+                    m => m.Type.Equals(CaDETMemberType.Method) || m.Type.Equals(CaDETMemberType.Constructor)))
+                {
+                    sb.Append(member.GetSignature()).Append("\n");
+                }
+            }
+            WriteToFile(sb.ToString(), "methods.txt");
+        }
+
+        private void WriteToFile(string text, string fileName)
+        {
+            File.WriteAllText(_resultFolder + fileName, text);
+        }
+
+        private void SaveWholeProjectToFile()
+        {
+            var sb = new StringBuilder();
+            foreach (var projectClass in _project.Classes)
+            {
+                sb.Append("\n\n").Append(projectClass.FullName).Append("\n");
+                sb.Append("Parent: ").Append(projectClass.Parent).Append("\n");
+                sb.Append("FIELDS\n");
+                foreach (var field in projectClass.Fields)
+                {
+                    sb.Append("  ").Append(field.Name).Append(",");
+                }
+                sb.Append("\n\nMETHODS\n");
+                foreach (var member in projectClass.Members)
+                {
+                    sb.Append("\n  ").Append(member).Append("\n  INVOKED METHODS\n");
+                    foreach (var im in member.InvokedMethods)
+                    {
+                        sb.Append("    ").Append(im).Append("\n");
+                    }
+
+                    sb.Append("  ACCESSED FIELDS\n");
+                    foreach (var im in member.AccessedFields)
+                    {
+                        sb.Append("    ").Append(im).Append("\n");
+                    }
+                }
+            }
+            WriteToFile(sb.ToString(), "whole.txt");
+        }
+    }
+}
--- PlatformInteractionTool/Program.cs
+++ PlatformInteractionTool/Program.cs
@@ -1,43 +1,13 @@
-﻿using System.IO;
-using System.Text;
-using RepositoryCompiler.CodeModel;
-using RepositoryCompiler.CodeModel.CaDETModel;
+﻿using PlatformInteractionTool.ModelInformationExtractors;
 
 namespace PlatformInteractionTool
 {
     class Program
     {
-        private static string _basePath = "C:\\student datasets\\";
-
         static void Main(string[] args)
         {
-            ParseStudentProjectsToFiles();
-        }
-
-        private static void ParseStudentProjectsToFiles()
-        {
-            CodeModelBuilder builder = new CodeModelBuilder(LanguageEnum.CSharp);
-
-            for (int i = 1; i <= 38; i++)
-            {
-                var project = builder.ParseFiles(_basePath + i);
-                SaveToFile(i, project);
-            }
-        }
-
-        private static void SaveToFile(int id, CaDETProject project)
-        {
-            var sb = new StringBuilder();
-            foreach (var projectClass in project.Classes)
-            {
-                sb.Append(projectClass.FullName).Append("\n");
-                sb.Append(projectClass.Metrics.LCOM).Append(",");
-                sb.Append(projectClass.Metrics.LOC).Append(",");
-                sb.Append(projectClass.Metrics.NAD).Append(",");
-                sb.Append(projectClass.Metrics.NMD).Append(",");
-                sb.Append(projectClass.Metrics.WMC).Append("\n");
-            }
-            File.WriteAllText(_basePath + id.ToString() + ".txt", sb.ToString());
+            var informationExtractor = new CaDETProjectExtractor("C:\\sdataset\\");
+            informationExtractor.ExtractNamesToFile();
         }
     }
 }
