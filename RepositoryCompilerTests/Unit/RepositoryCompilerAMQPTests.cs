07d032765ae14382cae50633c30cb18233257d66
vlaksi <daksrapsody@gmail.com>
2021-03-05 20:17:36 +0100

    test: message delivery on new queue

---
+++ RepositoryCompilerTests/Unit/RepositoryCompilerAMQPTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RepositoryCompiler.Communication;
+using Shouldly;
+using Xunit;
+
+namespace RepositoryCompilerTests.Unit
+{
+    public class RepositoryCompilerAMQPTests
+    {
+        [Fact]
+        public void Produce_Metrics_Report_Message()
+        {
+            RepositoryCompilerMessageProducer  producer = new RepositoryCompilerMessageProducer();
+            producer.CreateNewMetricsReport("string test");
+        }
+    }
+}
