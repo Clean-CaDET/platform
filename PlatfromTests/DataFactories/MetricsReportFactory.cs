a019b81add67f53536cb991b8fe878b695f90951
vlaksi <daksrapsody@gmail.com>
2021-03-07 18:15:54 +0100

    test: integration test for platform amqp communication between repository compiler, smell detector & smart tutor

---
+++ PlatfromTests/DataFactories/MetricsReportFactory.cs
@@ -0,0 +1,23 @@
+﻿using RepositoryCompiler.Communication;
+
+namespace Platform.DataFactories
+{
+    public class MetricsReportFactory
+    {
+        public CaDETClassDTO CreateMockupMetricsReportMessage()
+        {
+            CaDETClassDTO report = new CaDETClassDTO();
+
+            string exampleMethodId = "public void SavePerson(Person person);";
+
+            MetricsDTO metrics = new MetricsDTO();
+            metrics.LOC = 1000;
+            metrics.NOLV = 100;
+            metrics.NOP = 1;
+
+            report.CodeItemMetrics[exampleMethodId] = metrics;
+
+            return report;
+        }
+    }
+}
---
+++ PlatfromTests/Integration/PlatformAMQPTest.cs
@@ -0,0 +1,42 @@
+﻿using Shouldly;
+using Platform.DataFactories;
+using RepositoryCompiler.Communication;
+using SmartTutor.Communucation;
+using SmellDetector.Communication;
+using Xunit;
+using CaDETClassDTO =
+    RepositoryCompiler.Communication.CaDETClassDTO; // TODO: Decide where to storage same files between services
+
+namespace Platform.Integration
+{
+    // TODO: Find better place for this test class !
+    public class PlatformAMQPTest
+    {
+        private readonly MetricsReportFactory _metricsReportFactory = new MetricsReportFactory();
+
+        [Fact]
+        public void Produce_Metrics_Report_Message()
+        {
+            CreateMetricsReport();
+            CreateIssueReport();
+            ReadIssueReport();
+        }
+
+        private void CreateMetricsReport()
+        {
+            RepositoryCompilerMessageProducer producer = new RepositoryCompilerMessageProducer();
+            CaDETClassDTO reportMessage = _metricsReportFactory.CreateMockupMetricsReportMessage();
+            producer.CreateNewMetricsReport(reportMessage);
+        }
+
+        private void CreateIssueReport()
+        {
+            SmellDetectorMessageConsumer consumer = new SmellDetectorMessageConsumer();
+        }
+
+        private void ReadIssueReport()
+        {
+            SmartTutorMessageConsumer consumer = new SmartTutorMessageConsumer();
+        }
+    }
+}
