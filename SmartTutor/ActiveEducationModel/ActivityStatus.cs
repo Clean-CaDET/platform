45753a6e16dd7967d096ff7c486ea28fd7f3017d
Ana Atanackovic <anaatanackovic98@gmail.com>
2021-03-09 02:17:24 +0100

    feat[st]: Added classes for active education

---
+++ SmartTutor/ActiveEducationModel/ActivityStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartTutor.ActiveEducationModel
+{
+    public enum ActivityStatus
+    {
+        Locked,
+        Unlocked,
+        Started,
+        Completed
+    }
+}
---
+++ SmartTutor/ActiveEducationModel/EducationActivity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartTutor.ActiveEducationModel
+{
+    public class EducationActivity
+    {
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public ActivityStatus Status { get; set; }
+        public SmellType ActivityType { get; set; }
+    }
+}
