4efdce95b0f57f395c4c49351f9473a5582c0cc7
Ana Atanackovic <anaatanackovic98@gmail.com>
2021-03-09 02:38:56 +0100

    feat[st]: Added classes for active role in training and challenges

---
+++ SmartTutor/ActiveEducationModel/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartTutor.ActiveEducationModel
+{
+    public class Player
+    {
+        //public Person Person { get; set; }
+        public PlayerTitle Title { get; set; }
+        public double Score { get; set; }
+        public int Rank { get; set; }
+        public List<EducationActivity> EducationActivities { get; set; }
+    }
+}
---
+++ SmartTutor/ActiveEducationModel/PlayerTitle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartTutor.ActiveEducationModel
+{
+    public class PlayerTitle
+    {
+        public string Name { get; set; } //Novice,Beginner,Intermediate, Expert, Master
+        public double Points { get; set; }
+    }
+}
