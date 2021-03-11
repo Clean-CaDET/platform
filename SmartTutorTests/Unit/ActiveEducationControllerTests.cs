9059926f7ede937dcfd2cfc834b38ccd6b746031
Ana Atanackovic <anaatanackovic98@gmail.com>
2021-03-11 01:09:02 +0100

    test[st]: Counting of activities for the issue

---
+++ SmartTutorTests/Unit/ActiveEducationControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Shouldly;
+using SmartTutor.Controllers;
+using Xunit;
+
+namespace SmartTutorTests.Unit
+{
+    public class ActiveEducationControllerTests
+    {
+        [Fact]
+        public void Calculates_number_of_activities_for_issue()
+        {
+            ActiveEducationController activeEducationController = new ActiveEducationController();
+
+            var educationActivities = activeEducationController.FindActivitiesForIssue(SmellType.LONG_METHOD);
+
+            educationActivities.Count.ShouldBe(2);
+        }
+
+    }
+}
