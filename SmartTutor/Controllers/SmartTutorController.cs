d688f1d0522fa88dbbf407b6e2a5558097367013
Pufke <pualic.nemanja@gmail.com>
2021-03-17 01:01:48 +0100

    feature: first API Enpoint from plugin to SmartTutor

---
+++ SmartTutor/Controllers/SmartTutorController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartTutor.Controllers
+{
+    [Route("api/smarttutor")]
+    [ApiController]
+    public class SmarttutorController : ControllerBase
+    {
+        public SmarttutorController()
+        {
+      
+        }
+
+        [HttpPost("education/class")]
+        public string GetClassMetricsWithCohesionFeedback([FromBody] string classCode)
+        {
+            return "SUCC";
+        }
+    }
+}
