b4110288cc5b00bb9e298739a76d86c91b8dbada
Milica Siriski <milica.siriski998@gmail.com>
2021-03-17 14:28:47 +0100

    refact: Modified db context to use connection string only from configuration and deleted the integration test until we find a better solution for db context injection.

--- SmartTutor/ContentModel/SmartTutorContext.cs
+++ SmartTutor/ContentModel/SmartTutorContext.cs
@@ -4,21 +4,11 @@ namespace SmartTutor.ContentModel
 {
     public class SmartTutorContext : DbContext
     {
-        private const string CONNECTION_STRING = "User ID =postgres;Password=super;Server=localhost;Port=5432;Database=smart-tutor-db;Integrated Security=true;Pooling=true;";
         public DbSet<EducationalSnippet> EducationalSnippets { get; set; }
         public DbSet<EducationalContent> EducationalContents { get; set; }
 
-        public SmartTutorContext() : base()
-        {
-        }
-
         public SmartTutorContext(DbContextOptions<SmartTutorContext> options) : base(options)
         {
-        }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseNpgsql(CONNECTION_STRING);
         }
     }
 }
