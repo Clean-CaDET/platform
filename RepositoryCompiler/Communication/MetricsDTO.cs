b52536a6f864293d53a214536492c1d680dca4b3
Pufke <pualic.nemanja@gmail.com>
2021-03-18 02:03:51 +0100

    refactor: unused includes deleted

--- RepositoryCompiler/Communication/MetricsDTO.cs
+++ RepositoryCompiler/Communication/MetricsDTO.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using RepositoryCompiler.CodeModel.CaDETModel.Metrics;
+﻿using RepositoryCompiler.CodeModel.CaDETModel.Metrics;
 
 namespace RepositoryCompiler.Communication
 {
--- SmellDetector/Controllers/MetricsDTO.cs
+++ SmellDetector/Controllers/MetricsDTO.cs
@@ -1,5 +1,4 @@
-﻿using System;
-
+﻿
 namespace SmellDetector.Controllers
 {
     public class MetricsDTO
