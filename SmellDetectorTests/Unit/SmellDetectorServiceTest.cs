c664bd9646a871b1b749f2703f6a3c0e27463a32
Pufke <pualic.nemanja@gmail.com>
2021-03-19 01:44:22 +0100

    refactor: deleting redundant tests

--- SmellDetectorTests/Unit/SmellDetectorServiceTest.cs
+++ SmellDetectorTests/Unit/SmellDetectorServiceTest.cs
@@ -15,39 +15,6 @@ namespace SmellDetectorTests.Unit
     public class SmellDetectorServiceTest
     {
 
-        [Fact]
-        public void Generate_Smell_Detection_Report_For_LongMethod_And_Long_Parameter_List_Issues()
-        {
-            DetectionService detectionService = new DetectionService();
-            CaDETClassDTOFactory classFactory = new CaDETClassDTOFactory();
-
-            classFactory.CreateIssuesLongMethodAndLongParameterList();
-            var report = detectionService.GenerateSmellDetectionReport(classFactory.CaDETClassDTO);
-            report.Report[classFactory.TestIdentifier].Count().ShouldBe(classFactory.ExpectedIssues);
-        }
-
-        [Fact]
-        public void Generate_Smell_Detection_Report_For_LongMethod_Issue()
-        {
-            DetectionService detectionService = new DetectionService();
-            CaDETClassDTOFactory classFactory = new CaDETClassDTOFactory();
-
-            classFactory.CreateIssueLongMethod();
-            var report = detectionService.GenerateSmellDetectionReport(classFactory.CaDETClassDTO);
-            report.Report[classFactory.TestIdentifier].Count().ShouldBe(classFactory.ExpectedIssues);
-        }
-
-        [Fact]
-        public void Generate_Smell_Detection_Report_For_Another_LongMethod_Issue()
-        {
-            DetectionService detectionService = new DetectionService();
-            CaDETClassDTOFactory classFactory = new CaDETClassDTOFactory();
-
-            classFactory.CreateAnotherIssueLongMethod();
-            var report = detectionService.GenerateSmellDetectionReport(classFactory.CaDETClassDTO);
-            report.Report[classFactory.TestIdentifier].Count().ShouldBe(classFactory.ExpectedIssues);
-        }
-
         [Theory]
         [MemberData(nameof(Data))]
         public void Generate_Smell_Detection_Report(String testIdentifier, MetricsDTO metricsForIdentifier, int expectedIssues)
