41fbed9fbbb104b6e842394a68746a23159c8b72
Ana Atanacković <anaatanackovic98@gmail.com>
2021-03-25 15:41:53 +0100

    chore[st]: challenge as LO

---
+++ SmartTutor/ContentModel/LearningObjects/Challenge.cs
@@ -0,0 +1,11 @@
+﻿using RepositoryCompiler.CodeModel.CaDETModel.CodeItems;
+using System.Collections.Generic;
+
+namespace SmartTutor.ContentModel.LearningObjects
+{
+    public class Challenge : LearningObject
+    {
+        public string Url { get; set; }
+        public List<CaDETClass> EndState { get; set; }
+    }
+}
