8e817e1f250e547af8b8ca309ab5773628b54f1f
Simona Prokic <167simonap@gmail.com>
2021-03-26 18:17:07 +0100

    fix[RC]: Adds implementation details comments

--- RepositoryCompiler/CodeModel/CaDETModel/Metrics/CaDETClassMetrics.cs
+++ RepositoryCompiler/CodeModel/CaDETModel/Metrics/CaDETClassMetrics.cs
@@ -12,36 +12,43 @@
 
         /// <summary>
         /// NOR: Number of return statements
+        /// Implementation based on https://github.com/mauricioaniche/ck
         /// </summary>
         public int NOR { get; set; }
 
         /// <summary>
         /// NOL: Number of loops
+        /// Implementation based on https://github.com/mauricioaniche/ck
         /// </summary>
         public int NOL { get; set; }
 
         /// <summary>
         /// NOC: Number of comparison operators
+        /// Implementation based on https://github.com/mauricioaniche/ck
         /// </summary>
         public int NOC { get; set; }
 
         /// <summary>
         /// NOA: Number of assignments
+        /// Implementation based on https://github.com/mauricioaniche/ck
         /// </summary>
         public int NOA { get; set; }
 
         /// <summary>
         /// NOPM: Number of private methods
+        /// Implementation based on https://github.com/mauricioaniche/ck
         /// </summary>
         public int NOPM { get; set; }
 
         /// <summary>
         /// NOPF: Number of protected fields
+        /// Implementation based on https://github.com/mauricioaniche/ck
         /// </summary>
         public int NOPF { get; set; }
 
         /// <summary>
         /// MNB: Max nested blocks
+        /// Implementation based on https://github.com/mauricioaniche/ck
         /// </summary>
         public int MNB { get; set; }
     }
