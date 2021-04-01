f6da6dacdf4a579f7d4fb3b3664419a88c0a1cac
Nikola Luburic <luburic91@gmail.com>
2021-04-01 13:56:27 +0200

    feat: Adds questions to Naming lecture database script.

--- SmartTutor/Database/Scripts/learning-objects.sql
+++ SmartTutor/Database/Scripts/learning-objects.sql
@@ -4,6 +4,9 @@ DELETE FROM public."LearningObjects";
 DELETE FROM public."Texts";
 DELETE FROM public."Images";
 DELETE FROM public."Videos";
+DELETE FROM public."Challenges";
+DELETE FROM public."QuestionAnswers";
+DELETE FROM public."Questions";
 
 INSERT INTO public."LearningObjects"(
 	"Id", "LearningObjectSummaryId")
@@ -38,6 +41,27 @@ INSERT INTO public."LearningObjects"(
 INSERT INTO public."Images"(
 	"Id", "Url", "Caption")
 	VALUES (5, 'https://i.ibb.co/f144vCk/names-example.png', 'Ovako objektno orijentisani programer imenuje stvari kada izbegava reči iz poslovnog domena.');
+INSERT INTO public."LearningObjects"(
+	"Id", "LearningObjectSummaryId")
+	VALUES (17, 4);
+INSERT INTO public."Questions"(
+	"Id", "Text")
+	VALUES (17, 'Kada dizajniramo softver, bitan cilj je da razdvojimo funkcionalnosti poslovne logike (npr. pravilo kako se stornira kupovina) od logike koja je vezana za tehnološke detalje (npr. kod za formiranje HTTP zahteva). Ovo pravilo važi i za sama imena, gde objekti i funkcije poslovne logike treba da sadrže samo reči koje potiču iz domena problema, odnosno od naših klijenata. Označi imena funkcija koje smatraš da potiču iz domena poslovne logike:');
+INSERT INTO public."QuestionAnswers"(
+	"Id", "Text", "IsCorrect", "Feedback", "QuestionId")
+	VALUES (1, 'string.join(":", patientRecord)', false, 'Ovo nije ime iz domena problema već funkcija za spajanje elementa niza u string - tehnološki detalj.', 17);
+INSERT INTO public."QuestionAnswers"(
+	"Id", "Text", "IsCorrect", "Feedback", "QuestionId")
+	VALUES (2, 'Close(Account account)', true, 'Ovo ime je iz domena problema bankarskog poslovanja. Zatvaranje naloga je poslovna operacija koja se praktikovala i pre nego što su banke imale softver.', 17);
+INSERT INTO public."QuestionAnswers"(
+	"Id", "Text", "IsCorrect", "Feedback", "QuestionId")
+	VALUES (3, 'SaveToFile(MedicalHistory patientCard)', false, 'Ovo ime ističe tehnološki detalj - datoteku kao način perzistencije podataka. Da je ime metode bilo samo "Save" mogli bismo ga svrstati u domen problema kao poslovnu želju da se trajno sačuva ova informacija. Međutim, poslovnu logiku ne zanima da li je to čuvanje u datoteci, bazi podataka ili na cloud-u.', 17);
+INSERT INTO public."QuestionAnswers"(
+	"Id", "Text", "IsCorrect", "Feedback", "QuestionId")
+	VALUES (4, 'RefreshEmployeeRegistryView()', false, 'Po imenu možemo zaključiti da ova metoda osvežava neki prikaz (npr. tabele) što je tehnološki detalj.', 17);
+INSERT INTO public."QuestionAnswers"(
+	"Id", "Text", "IsCorrect", "Feedback", "QuestionId")
+	VALUES (5, 'RegisterMember(Member newMember)', true, 'Ovakva funkcija je deo poslovne logike oko registracije novog člana (npr. biblioteke). Naspram toga, operacija registracije korisnika aplikacije (npr. RegisterUser) bi podrazumevala tehnološku logiku koja bi mogla da uključi poslovnu ako bi se prilikom registracije formirao i član koji nije samo sistemski korisnik.', 17);
 	
 -- Naming - PK Node 1
 INSERT INTO public."LearningObjects"(
@@ -50,7 +74,7 @@ INSERT INTO public."Texts"(
 1. Opisivanje elementa koji želimo da imenujemo kroz rečenicu ili dve. Ovaj opis bi se mapirao na tekst koji bismo stavili u komentar uz element koji imenujemo. Tekst je lakše formirati na maternjem jeziku, a kada smo zadovoljni sa opisom možemo prevesti na engleski (sa ili bez pomoći onlajn rečnika).
 2. Uklanjanje svih redundantnih reči, veznika i priloga koji nisu ključni za prenošenje svrhe elementa koji imenujemo.
 3. Uklanjanje svih preostalih reči koje se lako izvlače iz **tipa** (npr. povratna vrednost, tip parametra, promenljive), **modula** kom dati element pripada (npr. kada imenujemo funkciju uzimamo u obzir ime klase, dok za klasu razmatramo paket) i preostalog konteksta (npr. parametri funkcije koju imenujemo).
-4. Razmatranje da li preostale reči adekvatno opisuju element i ako da
+4. Razmatranje da li preostale reči adekvatno opisuju element.
 5. Postavljanje novog imena.
 
 Navedeni algoritam nije jednokratna aktivnost. Često je potrebno nekoliko iteracija i preimenovanja da bismo stigli do imena koje je dovoljno jasno i značajno. Na sreću, savremena IDE rešenja pružaju podršku za lako preimenovanje elemenata koda. Ako kod prolazi *build* posle preimenovanja, možemo biti uvereni da nismo uveli nove greške u kod.');
@@ -80,8 +104,8 @@ INSERT INTO public."ChallengeFulfillmentStrategies"(
 	VALUES (2);
 -- TODO: NameStrategy and rules
 INSERT INTO public."Challenges"(
-	"Id", "Url", "FulfillmentStrategyId")
-	VALUES (12, 'https://github.com/Clean-CaDET/challenge-repository', 2);
+	"Id", "Url", "Description", "FulfillmentStrategyId")
+	VALUES (12, 'Često definišemo naša imena uz pomoć generičnih i beznačajnih reči koji ponavljaju jasnu informaciju ili ništa posebno ne kažu. U sklopu direktorijuma "01. Noise Words" isprati zadatke u zaglavlju klase i ukloni suvišne reči iz imena u kodu.', 'https://github.com/Clean-CaDET/challenge-repository', 2);
 	
 INSERT INTO public."LearningObjects"(
 	"Id", "LearningObjectSummaryId")
@@ -100,8 +124,8 @@ INSERT INTO public."ChallengeFulfillmentStrategies"(
 	VALUES (1);
 -- TODO: NameStrategy and rules
 INSERT INTO public."Challenges"(
-	"Id", "Url", "FulfillmentStrategyId")
-	VALUES (7, 'https://github.com/Clean-CaDET/challenge-repository', 1);
+	"Id", "Url", "Description", "FulfillmentStrategyId")
+	VALUES (7, 'U svojoj brzopletosti, često nabacamo kratka imena kako bismo što pre ispisali kod koji radi. U sklopu direktorijuma "02. Meaningful Words" proširi kod korisnim imenima koji uklanjaju potrebe za komentarima i isprati zadatke u zaglavlju klase.', 'https://github.com/Clean-CaDET/challenge-repository', 1);
 
 INSERT INTO public."LearningObjects"(
 	"Id", "LearningObjectSummaryId")
@@ -142,10 +166,29 @@ INSERT INTO public."Texts"(
 	"Id", "Content")
 	VALUES (16, 'Jasno ime objašnjava **šta** imenovano parče koda radi, ne **kako** to radi. Tako jedna funkcija može imati sledeća imena:
 
- - `List<KeyValuePair<EmployeeId, decimal>> SumBonusesAndSickDaysForSalaryCalculation`, gde iz imena možemo pretpostaviti kako izgleda kod;
+ - `List<KeyValuePair<EmployeeId, decimal>> SumBonusesAndSickDaysForSalaryCalculation()`, gde iz imena možemo pretpostaviti kako izgleda kod;
  - `List<KeyValuePair<EmployeeId, decimal>> ComputeSalariesPairList()`, gde ime redundantno opisuje tip povratne vrednosti i ništa van toga; i
  - `List<KeyValuePair<EmployeeId, decimal>> ComputeEmployeeSalaries()` gde je istaknuta suština operacije bez nepotrebnih dodatnih detalja.
 
 Iz perspektive programera koji radi sa ovim kodom (a inače je zadužen za drugu komponentu), razumemo nameru ovog koda bez da ulazimo u detalje vezane za korake ostvarivanja date namere ili izgleda konačnog rezultata (povratne vrednosti).
 
 Ime predstavlja nešto apstraktnije od skupa koraka i tipa povretne vrednost - rezultat je kolekcija plata zaposlenih, a ne bilo kakav spisak parova. Na isti način će neki `int` često predstavljati nešto više od `int` vrednosti - može da predstavlja godine osobe ili broj elemenata kolekcije i da postoje ograničenja koje vrednosti iz skupa svih celih brojeva može da uzme. Tako objekat tipa `Employee` može biti menadžer tima ili njegov član, dok lista `double` vrednost može da bude kolekcija merenja prosečne temperature u Novom Sadu u trenutnoj godini. U svim slučajevima značajna imena će nam pomoći da razumemo šta su ti podaci i ponašanje koje imenujemo.');
+
+INSERT INTO public."LearningObjects"(
+	"Id", "LearningObjectSummaryId")
+	VALUES (18, 14);
+INSERT INTO public."Questions"(
+	"Id", "Text")
+	VALUES (18, 'Ako tvrdimo da jasno ime objašnjava šta imenovano parče koda radi, a ne kako to radi, označi funkcije koje imaju jasno ime?');
+INSERT INTO public."QuestionAnswers"(
+	"Id", "Text", "IsCorrect", "Feedback", "QuestionId")
+	VALUES (1, 'Dictionary<MetricId, double>> ComputeMetricValueMap()', false, 'Ovo nije dobro ime, jer redundantno opisuje tip povratne vrednosti i ništa van toga. "GetMethodMetrics()" bi jasnije opisalo nameru funkcije.', 18);
+INSERT INTO public."QuestionAnswers"(
+	"Id", "Text", "IsCorrect", "Feedback", "QuestionId")
+	VALUES (2, 'void Save(Employee employee)', true, 'Ovo je dobro ime, pod pretpostavkom da se glagol "Save" po konvenciji koristi za svako čuvanje entiteta.', 18);
+INSERT INTO public."QuestionAnswers"(
+	"Id", "Text", "IsCorrect", "Feedback", "QuestionId")
+	VALUES (3, 'List<DatasetInstance> FindInstancesRequiringAdditionalAnnotation(Dataset dataset)', true, 'Ovo je dobro ime, jer apstrahuje detalje koji određuju šta znači da instanca nije dovoljno anotirana, a opet dodatno opisuje šta će ta povratna vrednost da predstavlja, odnosno da nije bilo kakva lista instanci.', 18);
+INSERT INTO public."QuestionAnswers"(
+	"Id", "Text", "IsCorrect", "Feedback", "QuestionId")
+	VALUES (4, 'decimal CalculatePriceFromOrderSizeAndDiscount(Order order, Discount discount)', false, 'Ovo nije dobro ime, jer opisuje kako se cena računa. Bolje ime bi bilo "CalculatePrice(Order order, Discount discount)" koje ističe nameru funkcije, a iz parametra se vidi da se popust uvažava.', 18);
