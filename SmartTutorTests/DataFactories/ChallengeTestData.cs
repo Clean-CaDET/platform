ebb8599c4072440ce2bbce0571f1f52665f60de6
Nikola Luburic <luburic91@gmail.com>
2021-04-06 10:42:45 +0200

    fix[st]: Reestablishes tests for MetricChecker and simplifies them using a Theory.

---
+++ SmartTutorTests/DataFactories/ChallengeTestData.cs
@@ -0,0 +1,80 @@
+﻿namespace SmartTutorTests.DataFactories
+{
+    class ChallengeTestData
+    {
+        public static string[] GetTwoPassingClasses()
+        {
+            return new[] {
+                @"using System;
+                namespace ExamplesApp.Method
+                {
+                   class Payment
+                   {
+    	               public int Cost { get; set; }
+    	               public bool IsExtra { get; set; }
+                   }
+                }",
+                @"using System;
+                namespace ExamplesApp.Method
+                {
+                    class PaymentService{
+	                    /// <summary>
+                        /// 1) Extract createPayment method.
+                        /// </summary>
+    	                private void CreatePayment(int price, int compensation) {
+		                    Payment payment = new Payment();
+		                    payment.Cost = price + compensation;
+                            payment.IsExtra = payment.Cost > 50000 ? true : false;
+      		                PrintPaymentDetails();
+    	                }
+	                    private void PrintPaymentDetails(int a) {
+      		                System.out.println(""Hello."");
+                            System.out.println(""Your payment is created."");
+                            System.out.println(""Cost is: "" + payment.Cost);
+                        }
+                    }
+                }"
+            };
+        }
+
+        public static string[] GetTwoViolatingClasses()
+        {
+            return new[]
+            {
+                @"using System;
+                namespace ExamplesApp.Method
+                {
+                   class Payment
+                   {
+    	               public int Cost { get; set; }
+    	               public bool IsExtra { get; set; }
+                   }
+                }",
+                @"using System;
+                namespace ExamplesApp.Method
+                {
+                    class PaymentService{
+	                    /// <summary>
+                        /// 1) Extract createPayment method.
+                        /// </summary>
+    	                private void CreatePayment(int price, int compensation) {
+		                    Payment payment = new Payment();
+		                    payment.Cost = price + compensation;
+                            payment.IsExtra = payment.Cost > 50000 ? true : false;
+
+      		                System.out.println(""Hello."");
+                            System.out.println(""Your payment is created."");
+                            System.out.println(""Cost is: "" + payment.Cost);
+      		                PrintPaymentDetails();
+    	                }
+	                    private void PrintPaymentDetails() {
+      		                System.out.println(""Hello."");
+                            System.out.println(""Your payment is created."");
+                            System.out.println(""Cost is: "" + payment.Cost);
+                        }
+                    }
+                }"
+            };
+        }
+    }
+}
--- SmartTutorTests/Unit/BasicMetricsCheckerTests.cs
+++ SmartTutorTests/Unit/BasicMetricsCheckerTests.cs
@@ -1,11 +1,12 @@
 ﻿using RepositoryCompiler.CodeModel.CaDETModel.CodeItems;
 using RepositoryCompiler.Controllers;
-using Shouldly;
 using SmartTutor.ContentModel.LearningObjects.ChallengeModel.FulfillmentStrategy;
 using SmartTutor.ContentModel.LearningObjects.ChallengeModel.FulfillmentStrategy.MetricChecker;
 using SmartTutor.ContentModel.LectureModel;
 using System.Collections.Generic;
 using System.Linq;
+using Shouldly;
+using SmartTutorTests.DataFactories;
 using Xunit;
 
 namespace SmartTutorTests.Unit
@@ -57,169 +58,36 @@ namespace SmartTutorTests.Unit
             };
         }
 
-        [Fact]
-        public void Checks_completed_challenge_fulfillment()
+        [Theory]
+        [MemberData(nameof(ChallengeTest))]
+        public void Evaluates_solution_submission(string[] submissionAttempt, List<ChallengeHint> expectedHints)
         {
-            string[] sourceCode = new string[] {
-                @"using System;
-                namespace ExamplesApp.Method
-                {
-                   class Payment
-                   {
-    	               public int Cost { get; set; }
-    	               public bool IsExtra { get; set; }
-                   }
-                }",
-                @"using System;
-                namespace ExamplesApp.Method
-                {
-                    class PaymentService{
-	                    /// <summary>
-                        /// 1) Extract createPayment method.
-                        /// </summary>
-    	                private void CreatePayment(int price, int compensation) {
-		                    Payment payment = new Payment();
-		                    payment.Cost = price + compensation;
-                            payment.IsExtra = payment.Cost > 50000 ? true : false;
-      		                PrintPaymentDetails();
-    	                }
-	                    private void PrintPaymentDetails() {
-      		                System.out.println(""Hello."");
-                            System.out.println(""Your payment is created."");
-                            System.out.println(""Cost is: "" + payment.Cost);
-                        }
-                    }
-                }"
-            };
+            var caDETClasses = new CodeRepositoryService().BuildClassesModel(submissionAttempt);
+            var challengeEvaluation = _basicMetricsChecker.EvaluateSubmission(caDETClasses);
+            var actualHints = challengeEvaluation.GetHints().Values.SelectMany(h => h).ToList();
 
-            HintDirectory challengeEvaluation = _basicMetricsChecker.EvaluateSubmission(new CodeRepositoryService().BuildClassesModel(sourceCode));
-
-            
+            actualHints.Count.ShouldBe(expectedHints.Count);
+            actualHints.All(expectedHints.Contains).ShouldBeTrue();
         }
 
-        [Fact]
-        public void Checks_incompleted_challenge_fulfillment()
-        {
-            string[] sourceCode = new string[] {
-                @"using System;
-                namespace ExamplesApp.Method
+        public static IEnumerable<object[]> ChallengeTest =>
+            new List<object[]>
+            {
+                new object[]
                 {
-                   class Payment
-                   {
-    	               public int Cost { get; set; }
-    	               public bool IsExtra { get; set; }
-                   }
-                }",
-                @"using System;
-                namespace ExamplesApp.Method
+                    //TODO: Find better names for these methods and add more test data and more thorough HintDirectory evaluation (probably separate tests for MetricRangeRules and HintDirectory)
+                    ChallengeTestData.GetTwoPassingClasses(),
+                    new List<ChallengeHint>()
+                },
+                new object[]
                 {
-                    class PaymentService{
-	                    /// <summary>
-                        /// 1) Extract createPayment method.
-                        /// </summary>
-    	                private void CreatePayment(int price, int compensation) {
-		                    Payment payment = new Payment();
-		                    payment.Cost = price + compensation;
-                            payment.IsExtra = payment.Cost > 50000 ? true : false;
-
-      		                System.out.println(""Hello."");
-                            System.out.println(""Your payment is created."");
-                            System.out.println(""Cost is: "" + payment.Cost);
-      		                PrintPaymentDetails();
-    	                }
-	                    private void PrintPaymentDetails() {
-      		                System.out.println(""Hello."");
-                            System.out.println(""Your payment is created."");
-                            System.out.println(""Cost is: "" + payment.Cost);
-                        }
+                    ChallengeTestData.GetTwoViolatingClasses(),
+                    new List<ChallengeHint>
+                    {
+                        new ChallengeHint {Id = 6},
+                        new ChallengeHint {Id = 337002}
                     }
-                }"
+                }
             };
-
-            HintDirectory challengeEvaluation = _basicMetricsChecker.EvaluateSubmission(new CodeRepositoryService().BuildClassesModel(sourceCode));
-
-        }
-
-        [Fact]
-        public void Gets_challenge_hints_for_complete_solution_attempt()
-        {
-            string[] sourceCode = new string[] {
-                @"using System;
-                namespace ExamplesApp.Method
-                {
-                   class Payment
-                   {
-    	               public int Cost { get; set; }
-    	               public bool IsExtra { get; set; }
-                   }
-                }",
-                @"using System;
-                namespace ExamplesApp.Method
-                {
-                    class PaymentService{
-	                    /// <summary>
-                        /// 1) Extract createPayment method.
-                        /// </summary>
-    	                private void CreatePayment(int price, int compensation) {
-		                    Payment payment = new Payment();
-		                    payment.Cost = price + compensation;
-                            payment.IsExtra = payment.Cost > 50000 ? true : false;
-      		                PrintPaymentDetails();
-    	                }
-	                    private void PrintPaymentDetails() {
-      		                System.out.println(""Hello."");
-                            System.out.println(""Your payment is created."");
-                            System.out.println(""Cost is: "" + payment.Cost);
-                        }
-                    }
-                }"
-            };
-            List<CaDETClass> caDETClasses = new CodeRepositoryService().BuildClassesModel(sourceCode);
-            HintDirectory challengeHints = _basicMetricsChecker.EvaluateSubmission(caDETClasses);
-
-        }
-
-        [Fact]
-        public void Gets_challenge_hints_for_incomplete_solution_attempt()
-        {
-            string[] sourceCode = new string[] {
-                @"using System;
-                namespace ExamplesApp.Method
-                {
-                   class Payment
-                   {
-    	               public int Cost { get; set; }
-    	               public bool IsExtra { get; set; }
-                   }
-                }",
-                @"using System;
-                namespace ExamplesApp.Method
-                {
-                    class PaymentService{
-	                    /// <summary>
-                        /// 1) Extract createPayment method.
-                        /// </summary>
-    	                private void CreatePayment(int price, int compensation) {
-		                    Payment payment = new Payment();
-		                    payment.Cost = price + compensation;
-                            payment.IsExtra = payment.Cost > 50000 ? true : false;
-
-      		                System.out.println(""Hello."");
-                            System.out.println(""Your payment is created."");
-                            System.out.println(""Cost is: "" + payment.Cost);
-      		                PrintPaymentDetails();
-    	                }
-	                    private void PrintPaymentDetails() {
-      		                System.out.println(""Hello."");
-                            System.out.println(""Your payment is created."");
-                            System.out.println(""Cost is: "" + payment.Cost);
-                        }
-                    }
-                }"
-            };
-            List<CaDETClass> caDETClasses = new CodeRepositoryService().BuildClassesModel(sourceCode);
-            HintDirectory challengeHints = _basicMetricsChecker.EvaluateSubmission(caDETClasses);
-
-        }
     }
 }
