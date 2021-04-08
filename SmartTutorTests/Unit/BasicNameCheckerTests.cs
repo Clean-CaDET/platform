0c7fa87275df0bd2149dae422e00c796832a59d1
Ana Atanacković <anaatanackovic98@gmail.com>
2021-04-08 18:29:37 +0200

    test[st]: evaluation of submitted classes for naming strategy

--- SmartTutorTests/Unit/BasicNameCheckerTests.cs
+++ SmartTutorTests/Unit/BasicNameCheckerTests.cs
@@ -1,6 +1,11 @@
-﻿using SmartTutor.ContentModel.LearningObjects.ChallengeModel.FulfillmentStrategy;
+﻿using RepositoryCompiler.Controllers;
+using Shouldly;
+using SmartTutor.ContentModel.LearningObjects.ChallengeModel.FulfillmentStrategy;
 using SmartTutor.ContentModel.LearningObjects.ChallengeModel.FulfillmentStrategy.NameChecker;
+using SmartTutorTests.DataFactories;
 using System.Collections.Generic;
+using System.Linq;
+using Xunit;
 
 namespace SmartTutorTests.Unit
 {
@@ -24,16 +29,50 @@ namespace SmartTutorTests.Unit
                             Id = 337003,
                             Content = "Cohesion",
                             LearningObjectSummaryId = 336
-                        }
+                        },
+                        MinLength = 3
                     },
                     new NamingRule
                     {
                         Id = 3370002,
                         BannedWords = new List<string> (),
-                        RequiredWords = new List<string> { "Create", "Payment", "price" }
+                        RequiredWords = new List<string> { "Create", "Payment", "price" },
+                        Hint = new ChallengeHint { Id = 7 }
                     }
                 }
             };
         }
+
+        [Theory]
+        [MemberData(nameof(ChallengeTest))]
+        public void Evaluates_solution_submission(string[] submissionAttempt, List<ChallengeHint> expectedHints)
+        {
+            var caDETClasses = new CodeRepositoryService().BuildClassesModel(submissionAttempt);
+            var challengeEvaluation = _basicNameChecker.EvaluateSubmission(caDETClasses);
+            var actualHints = challengeEvaluation.GetHints();
+
+            actualHints.Count.ShouldBe(expectedHints.Count);
+            actualHints.All(expectedHints.Contains).ShouldBeTrue();
+        }
+
+        public static IEnumerable<object[]> ChallengeTest =>
+            new List<object[]>
+            {
+                new object[]
+                {
+                    //TODO: Find better names for these methods and add more test data and more thorough HintDirectory evaluation (probably separate tests for MetricRangeRules and HintDirectory)
+                    ChallengeTestData.GetTwoPassingClasses(),
+                    new List<ChallengeHint>()
+                },
+                new object[]
+                {
+                    ChallengeTestData.GetTwoViolatingClasses(),
+                    new List<ChallengeHint>
+                    {
+                        new ChallengeHint {Id = 7},
+                        new ChallengeHint {Id = 337003}
+                    }
+                }
+            };
     }
 }
