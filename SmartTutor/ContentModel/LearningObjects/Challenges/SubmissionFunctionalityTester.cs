056ee2ff08b38ca683cce76c25461d9a37c2da90
Nikola Luburic <luburic91@gmail.com>
2021-04-21 17:09:29 +0200

    feat[st]: Checks code for compilation errors.

---
+++ SmartTutor/ContentModel/LearningObjects/Challenges/SubmissionFunctionalityTester.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SmartTutor.ContentModel.LearningObjects.ChallengeModel
+{
+    public class ChallengeSubmissionFunctionalityTester
+    {
+        public List<string> Test(string sourceCode)
+        {
+            var syntaxErrors = new List<string>();
+            var syntaxTree = CSharpSyntaxTree.ParseText(sourceCode);
+            
+            var diagnostics = syntaxTree.GetDiagnostics().ToList();
+            if (diagnostics.Count > 0)
+            {
+                foreach (var diagnostic in diagnostics)
+                {
+                    syntaxErrors.Add(diagnostic.ToString());
+                }
+            }
+
+            return syntaxErrors;
+        }
+    }
+}
