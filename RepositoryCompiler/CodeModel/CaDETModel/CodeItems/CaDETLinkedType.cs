7fb2a87c442f43ae5e7913ed9dbbe9f5b9afa388
Simona Prokic <167simonap@gmail.com>
2021-04-22 15:59:03 +0200

    feat[RC]: Adds CaDETLinkedType and CaDETVariable classes

---
+++ RepositoryCompiler/CodeModel/CaDETModel/CodeItems/CaDETLinkedType.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace RepositoryCompiler.CodeModel.CaDETModel.CodeItems
+{
+    public class CaDETLinkedType
+    {
+        public string FullType { get; set; }
+        public List<CaDETClass> LinkedTypes { get; set; }
+    }
+}
