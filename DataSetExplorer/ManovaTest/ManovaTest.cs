3c1f1282c2110a78e81c50a935a648fc5288fa30
Simona Prokic <167simonap@gmail.com>
2021-04-29 20:52:05 +0200

    feat[DE]: Adds MANOVA test

---
+++ DataSetExplorer/ManovaTest/ManovaTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using RepositoryCompiler.CodeModel.CaDETModel.CodeItems;
+
+namespace DataSetExplorer.ManovaTest
+{
+    class ManovaTest
+    {
+        private readonly string _manovaScriptFile = "../../../ManovaTest/manova_test.py";
+        private readonly string _pythonPath = "../../../ManovaTest/venv/Scripts/python.exe";
+        private string _annotatedInstancesFile;
+        private string _metrics;
+        
+        public void SetupTestArguments(string annotatedInstancesFile, List<CaDETMetric> metrics)
+        {
+            _annotatedInstancesFile = annotatedInstancesFile;
+            _metrics = "";
+            foreach (var metric in metrics)
+            {
+                _metrics += metric + " + ";
+            }
+            _metrics = _metrics.Substring(0, _metrics.Length - 2);
+        }
+
+        public void RunTest()
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = _pythonPath;
+            startInfo.Arguments = $"{_manovaScriptFile} {_annotatedInstancesFile} \"{_metrics}\"";
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardOutput = true;
+            using (Process process = Process.Start(startInfo))
+            {
+                using (StreamReader reader = process.StandardOutput)
+                {
+                    string result = reader.ReadToEnd();
+                    Console.Write(result);
+                }
+            }
+        }
+    }
+}
---
+++ DataSetExplorer/ManovaTest/manova_test.py
@@ -0,0 +1,11 @@
+import sys
+import pandas as pd
+from statsmodels.multivariate.manova import MANOVA
+
+
+if __name__ == '__main__':
+    data_file_path = sys.argv[1]
+    df = pd.read_excel(data_file_path)
+    dependent_variables = sys.argv[2]
+    maov = MANOVA.from_formula(dependent_variables + '~ Annotation', data=df)
+    print(maov.mv_test())
