e75763de044126b566d1a36f492ef6608ff7868f
katarina-glorija <katarina.glorija.grujic@gmail.com>
2021-05-13 15:15:16 +0200

    Implemented model for hybrid rule creation

--- SmellDetector/Detectors/RuleEngines/ClassHybridRuleEngine.cs
+++ SmellDetector/Detectors/RuleEngines/ClassHybridRuleEngine.cs
@@ -1,7 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using CodeModel.CaDETModel.CodeItems;
 using SmellDetector.SmellModel;
 using SmellDetector.SmellModel.Reports;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SmellDetector.Detectors.RuleEngines
 {
@@ -18,33 +19,51 @@ namespace SmellDetector.Detectors.RuleEngines
             _relatedSmellsCount = 5;
         }
 
-        public PartialSmellDetectionReport FindIssues(List<CaDETClassDTO> caDetClassDtoList)
+        public PartialSmellDetectionReport FindIssues(List<CaDETClass> caDetClassList)
         {
+            PartialSmellDetectionReport partialReport = new();
 
-            PartialSmellDetectionReport partialReport = new PartialSmellDetectionReport();
-
-            foreach (CaDETClassDTO caDETClassDTO in caDetClassDtoList)
+            foreach (CaDETClass caDETClass in caDetClassList)
             {
-                List<Issue> issues = ApplyRule(caDETClassDTO);
-                foreach (Issue issue in issues)
+                Issue metricRuleIssue = ApplyRule(caDETClass);
+                if(metricRuleIssue != null)
                 {
-                    if (issue != null)
+                    PartialSmellDetectionReport methodIssuesReport = CreateMethodsIssuesReport(caDETClass);
+                    if(ClassHasEnoughLongMethods(methodIssuesReport))
                     {
-                        partialReport.AddIssue(issue.CodeSnippetId, issue);
+                        partialReport.AddIssue(caDETClass.FullName,
+                                                new() { IssueType = SmellType.GOD_CLASS, CodeSnippetId = caDETClass.FullName });
                     }
                 }
             }
             return partialReport;
-            _rule.Validate()
-            //if(cloc>300) call method rule engine -> partial issue if smelltype == longmethod ++brojac
-            //if brojac == 5
-            // return true partial...
-            throw new NotImplementedException();
         }
 
-        private Issue ApplyRule(CaDETClassDTO c)
+        private PartialSmellDetectionReport CreateMethodsIssuesReport(CaDETClass caDETClass)
         {
-            return _rule.Validate(c.FullName, c.CodeSnippetMetrics);
+            MethodMetricRuleEngine methodMetricRuleEngine = new();
+            PartialSmellDetectionReport report = methodMetricRuleEngine.FindIssues(new List<CaDETClass> { caDETClass });
+            return report;
+        }
+
+        private bool ClassHasEnoughLongMethods(PartialSmellDetectionReport report)
+        {
+            int longMethodCounter = 0;
+            foreach (List<Issue> issues in report.CodeItemIssues.Values)
+            {
+                longMethodCounter += issues.Select(issue => issue.IssueType == SmellType.LONG_METHOD).Count();
+                if (longMethodCounter >= _relatedSmellsCount)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private Issue ApplyRule(CaDETClass c)
+        {
+            return _rule.Validate(c.FullName, c.Metrics);
         }
     }
 }
---
+++ SmellDetector/Detectors/RuleEngines/MethodMetricRuleEngine.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CodeModel.CaDETModel.CodeItems;
+using SmellDetector.SmellModel;
+using SmellDetector.SmellModel.Reports;
+
+namespace SmellDetector.Detectors.RuleEngines
+{
+    public class MethodMetricRuleEngine : IDetector
+    {
+
+        private readonly List<Rule> _rules;
+        private readonly List<Rule> _dynamicRules;
+
+        public MethodMetricRuleEngine()
+        {
+            _rules = new List<Rule>();
+            _dynamicRules = new List<Rule>();
+            Rule rule1 = new Rule("10.1109/SCAM.2013.6648192",
+                                  new MetricCriteria(CaDETMetric.MLOC, OperationEnum.GREATER_THAN, 50),
+                                  SmellType.LONG_METHOD);
+            Rule rule2 = new Rule("10.1016/j.jss.2006.10.018",
+                                  new OrCriteria(
+                                      new MetricCriteria(CaDETMetric.MLOC, OperationEnum.GREATER_THAN, 50),
+                                      new MetricCriteria(CaDETMetric.CYCLO, OperationEnum.GREATER_THAN, 10)),
+                                  SmellType.LONG_METHOD);
+            Rule rule3 = new Rule("https://doi.org/10.1145/3132498.3134268",
+                                  new AndCriteria(
+                                      new AndCriteria(
+                                          new MetricCriteria(CaDETMetric.MLOC, OperationEnum.GREATER_THAN, 30),
+                                          new MetricCriteria(CaDETMetric.CYCLO, OperationEnum.GREATER_THAN, 4)),
+                                      new MetricCriteria(CaDETMetric.MMNB, OperationEnum.GREATER_THAN, 3)),
+                                  SmellType.LONG_METHOD);
+        }
+
+        public PartialSmellDetectionReport FindIssues(List<CaDETClass> classes)
+        {
+            List<CaDETMember> methods = classes.SelectMany(c => c.Members).ToList();
+
+            var partialReport = new PartialSmellDetectionReport();
+
+            foreach (var method in methods)
+            {
+                var issues = ApplyRule(method);
+                foreach (var issue in issues.Where(issue => issue != null))
+                {
+                    partialReport.AddIssue(issue.CodeSnippetId, issue);
+                }
+            }
+            return partialReport;
+        }
+
+        private List<Issue> ApplyRule(CaDETMember m)
+        {
+            List<Issue> issues = _rules.Select(r => r.Validate(m.Name, m.Metrics)).ToList();
+            issues.AddRange(_dynamicRules.Select(r => r.Validate(m.Name, m.Metrics)).ToList());
+            return issues;
+        }
+    }
+}
