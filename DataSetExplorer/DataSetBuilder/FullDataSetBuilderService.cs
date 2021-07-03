ea0767b9a63b6ebbfeab04c4a7fdb1d74a8bb252
Simona Prokic <167simonap@gmail.com>
2021-07-03 13:50:53 +0200

    feat[DE]: Console app with configurable input

--- DataSetExplorer/DataSetBuilder/FullDataSetBuilderService.cs
+++ DataSetExplorer/DataSetBuilder/FullDataSetBuilderService.cs
@@ -10,17 +10,25 @@ namespace DataSetExplorer
 {
     class FullDataSetBuilderService : IFullDataSetBuilder
     {
+        private ListDictionary _projects;
+        private List<Annotator> _annotators;
+
+        public FullDataSetBuilderService(ListDictionary projects, List<Annotator> annotators)
+        {
+            _projects = projects;
+            _annotators = annotators;
+        }
+
         public IEnumerable<IGrouping<string, DataSetInstance>> GetAnnotatedInstancesGroupedBySmells(int? annotatorId)
         {
             var allAnnotatedInstances = new List<DataSetInstance>();
 
-            var projects = GetAnnotatedProjects();
-            foreach (var key in projects.Keys)
+            foreach (var key in _projects.Keys)
             {
                 CodeModelFactory factory = new CodeModelFactory();
                 CaDETProject project = factory.CreateProjectWithCodeFileLinks(key.ToString());
 
-                var importer = new ExcelImporter(projects[key].ToString());
+                var importer = new ExcelImporter(_projects[key].ToString());
                 var annotatedInstances = importer.Import("Clean CaDET").GetAllInstances();
 
                 LoadAnnotators(ref annotatedInstances);
@@ -38,32 +46,11 @@ namespace DataSetExplorer
             }).ToList();
         }
 
-        private ListDictionary GetAnnotatedProjects()
-        {
-            ListDictionary projects = new ListDictionary(); // local repository path, annotations folder path
-            projects.Add("D:/ccadet/annotations/repos/BurningKnight", "D:/ccadet/annotations/annotated/BurningKnight");
-            projects.Add("D:/ccadet/annotations/repos/Core2D", "D:/ccadet/annotations/annotated/Core2d");
-            projects.Add("D:/ccadet/annotations/repos/jellyfin", "D:/ccadet/annotations/annotated/Jellyfin");
-            projects.Add("D:/ccadet/annotations/repos/OpenRA", "D:/ccadet/annotations/annotated/OpenRA");
-            projects.Add("D:/ccadet/annotations/repos/ShareX", "D:/ccadet/annotations/annotated/ShareX");
-            projects.Add("D:/ccadet/annotations/repos/ShopifySharp", "D:/ccadet/annotations/annotated/ShopifySharp");
-            projects.Add("D:/ccadet/annotations/repos/MonoGame", "D:/ccadet/annotations/annotated/MonoGame");
-            projects.Add("D:/ccadet/annotations/repos/Osu", "D:/ccadet/annotations/annotated/Osu");
-            return projects;
-        }
-
         private void LoadAnnotators(ref List<DataSetInstance> annotatedInstances)
         {
-            List<Annotator> annotators = new List<Annotator>()
-            {
-                new Annotator(1, 6, 1),
-                new Annotator(2, 2, 2),
-                new Annotator(3, 2, 3)
-            };
-
             foreach (var annotation in annotatedInstances.SelectMany(i => i.Annotations))
             {
-                annotation.Annotator = annotators.Find(annotator => annotator.Id.Equals(annotation.Annotator.Id));
+                annotation.Annotator = _annotators.Find(annotator => annotator.Id.Equals(annotation.Annotator.Id));
             }
         }
     }
--- DataSetExplorer/Program.cs
+++ DataSetExplorer/Program.cs
@@ -1,26 +1,254 @@
-﻿using DataSetExplorer.RepositoryAdapters;
+﻿using DataSetExplorer.DataSetBuilder.Model;
+using DataSetExplorer.RepositoryAdapters;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
 
 namespace DataSetExplorer
 {
     class Program
     {
+        private static IDataSetAnalyzer _dataSetAnalysisService = new DataSetAnalysisService();
+        
         static void Main(string[] args)
         {
-            //IDataSetCreator dataSetCreationService = new DataSetCreationService("C:\\main-repository\\", new GitCodeRepository());
-            //dataSetCreationService.CreateDataSetSpreadsheet("MyProject", "https://github.com/wieslawsoltes/Core2D/tree/50ea0849bef7e211500764ffcd1fece4a3cb224e");
+            string chosenOption;
+            do
+            {
+                WriteMenuWithOptions();
+                chosenOption = GetAnswerOnQuestion("Your option: ");
+                Console.Clear();
+                ProcessChosenOption(chosenOption);
 
-            //IDataSetAnalyzer dataSetAnalysisService = new DataSetAnalysisService();
-            //dataSetAnalysisService.FindInstancesRequiringAdditionalAnnotationUseCase("D:/ccadet/annotations/annotated/ShopifySharp/", "D:/ccadet/annotations/additional/RequireAdditionalAnnotations");
-            //dataSetAnalysisService.FindInstancesWithAllDisagreeingAnnotationsUseCase("D:/ccadet/annotations/annotated/ShopifySharp/", "D:/ccadet/annotations/additional/DisagreeingAnnotations");
+            } while (!chosenOption.Equals("x"));
+        }
 
-            IDataSetExporter dataSetExportationService = new DataSetExportationService(new FullDataSetBuilderService());
-            dataSetExportationService.Export("D:/ccadet/annotations/annotated/Output/");
+        private static void ProcessChosenOption(string chosenOption)
+        {
+            switch (chosenOption)
+            {
+                case "1":
+                    CreateDataSet();
+                    break;
+                case "2":
+                    AnalyzeDataSet();
+                    break;
+                case "3":
+                    ExportDataSet();
+                    break;
+                case "4":
+                    CheckAnnotationsConsistency();
+                    break;
+                case "x":
+                    break;
+                default:
+                    ChosenOptionErrorMessage(chosenOption);
+                    break;
+            }
+        }
 
-            /*IAnnotationConsistencyChecker annotationConsistencyService = new AnnotationConsistencyService(new FullDataSetBuilderService());
-            annotationConsistencyService.CheckAnnotationConsistencyForAnnotator(1);
-            annotationConsistencyService.CheckAnnotationConsistencyBetweenAnnotatorsForSeverity(1);
-            annotationConsistencyService.CheckMetricsSignificanceInAnnotationsForAnnotator(1);
-            annotationConsistencyService.CheckMetricsSignificanceBetweenAnnotatorsForSeverity(0);*/
+        private static void CreateDataSet()
+        {
+            string outputPath = GetAnswerOnQuestion("Enter output folder path: ");
+            IDataSetCreator _dataSetCreationService = new DataSetCreationService(outputPath, new GitCodeRepository());
+
+            var projects = GetProjectsForDataSet();
+            foreach(var projectName in projects.Keys)
+            {
+                _dataSetCreationService.CreateDataSetSpreadsheet(projectName.ToString(), projects[projectName].ToString());
+            }
+        }
+
+        private static ListDictionary GetProjectsForDataSet()
+        {
+            ListDictionary projects = new ListDictionary();
+            string projectName;
+            string projectAndCommitUrl;
+            string finishOption;
+
+            Console.WriteLine("\nProjects for data set:");
+            do
+            {
+                projectName = GetAnswerOnQuestion("Enter project name: ");
+                projectAndCommitUrl = GetAnswerOnQuestion("Enter project and commit URL: ");
+                projects.Add(projectName, projectAndCommitUrl);
+                finishOption = GetAnswerOnQuestion("Finished? (y/n): ");
+            } while (finishOption.Equals("n"));
+
+            return projects;
+        }
+
+        private static void AnalyzeDataSet()
+        {
+            string dataSetPath;
+            string outputPath;
+            string finishOption;
+
+            do
+            {
+                dataSetPath = GetAnswerOnQuestion("Enter data set folder path: ");
+                outputPath = GetAnswerOnQuestion("Enter output folder path: ");
+                ChooseAnalyzeDataSetOption(dataSetPath, outputPath);
+                finishOption = GetAnswerOnQuestion("Finished? (y/n): ");
+            } while (finishOption.Equals("n"));
+        }
+
+        private static void ChooseAnalyzeDataSetOption(string dataSetPath, string outputPath)
+        {
+            string chosenOption;
+            do
+            {
+                WriteAnalyzeDataSetOptions();
+                chosenOption = GetAnswerOnQuestion("Your option: ");
+
+                switch (chosenOption)
+                {
+                    case "1":
+                        _dataSetAnalysisService.FindInstancesRequiringAdditionalAnnotationUseCase(dataSetPath, outputPath);
+                        break;
+                    case "2":
+                        _dataSetAnalysisService.FindInstancesWithAllDisagreeingAnnotationsUseCase(dataSetPath, outputPath);
+                        break;
+                    case "x":
+                        break;
+                    default:
+                        ChosenOptionErrorMessage(chosenOption);
+                        break;
+                }
+            } while (!chosenOption.Equals("x"));
+        }
+
+        private static void ExportDataSet()
+        {
+            var projects = GetAnnotatedProjects();
+            var annotators = GetAnnotators();
+            IDataSetExporter dataSetExportationService = new DataSetExportationService(new FullDataSetBuilderService(projects, annotators));
+
+            string outputPath = GetAnswerOnQuestion("Enter output folder path: ");
+            dataSetExportationService.Export(outputPath);
+        }
+
+        private static ListDictionary GetAnnotatedProjects()
+        {
+            ListDictionary projects = new ListDictionary();
+            string localRepoPath;
+            string annotationsFolderPath;
+            string finishOption;
+
+            Console.WriteLine("Projects:");
+            do
+            {
+                localRepoPath = GetAnswerOnQuestion("Enter local repository folder path: ");
+                annotationsFolderPath = GetAnswerOnQuestion("Enter annotations folder path: ");
+                projects.Add(localRepoPath, annotationsFolderPath);
+                finishOption = GetAnswerOnQuestion("Finished? (y/n): ");
+            } while (finishOption.Equals("n"));
+
+            return projects;
+        }
+
+        private static List<Annotator> GetAnnotators()
+        {
+            List<Annotator> annotators = new List<Annotator>();
+            string annotatorsFilePath = GetAnswerOnQuestion("Enter annotators file path: ");
+
+            string[] lines = File.ReadAllLines(annotatorsFilePath);
+            foreach (string line in lines)
+            {
+                string[] columns = line.Split(',');
+                annotators.Add(new Annotator(int.Parse(columns[0]), int.Parse(columns[1]), int.Parse(columns[2])));
+            }
+            return annotators;
+        }
+
+        private static void CheckAnnotationsConsistency()
+        {
+            var projects = GetAnnotatedProjects();
+            var annotators = GetAnnotators();
+
+            IAnnotationConsistencyChecker annotationConsistencyService = new AnnotationConsistencyService(new FullDataSetBuilderService(projects, annotators));
+
+            string chosenOption;
+            do
+            {
+                WriteAnnotationsConsistencyOptions();
+                chosenOption = GetAnswerOnQuestion("Your option: ");
+
+                switch (chosenOption)
+                {
+                    case "1":
+                        var annotatorId = GetId("Annotator");
+                        if (annotatorId.HasValue) annotationConsistencyService.CheckAnnotationConsistencyForAnnotator(annotatorId.Value);
+                        break;
+                    case "2":
+                        var severityId = GetId("Severity");
+                        if (severityId.HasValue) annotationConsistencyService.CheckAnnotationConsistencyBetweenAnnotatorsForSeverity(severityId.Value);
+                        break;
+                    case "3":
+                        annotatorId = GetId("Annotator");
+                        if (annotatorId.HasValue) annotationConsistencyService.CheckMetricsSignificanceInAnnotationsForAnnotator(annotatorId.Value);
+                        break;
+                    case "4":
+                        severityId = GetId("Severity");
+                        if (severityId.HasValue) annotationConsistencyService.CheckMetricsSignificanceBetweenAnnotatorsForSeverity(severityId.Value);
+                        break;
+                    case "x":
+                        break;
+                    default:
+                        ChosenOptionErrorMessage(chosenOption);
+                        break;
+                }
+            } while (!chosenOption.Equals("x"));
+        }
+
+        private static int? GetId(string name)
+        {
+            string id = GetAnswerOnQuestion(name + " ID: ");
+            if (int.TryParse(id, out int parsedId)) return parsedId;
+            Console.Write("Invalid ID: ID must be an integer.");
+            return null;
+        }
+
+        private static string GetAnswerOnQuestion(string question)
+        {
+            Console.Write(question);
+            return Console.ReadLine();
+        }
+
+        private static void WriteMenuWithOptions()
+        {
+            Console.WriteLine("DATASET EXPLORER\n");
+
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1. Create dataset");
+            Console.WriteLine("2. Analyze dataset");
+            Console.WriteLine("3. Export dataset");
+            Console.WriteLine("4. Check annotations consistency");
+            Console.WriteLine("x. Exit\n");
+        }
+
+        private static void WriteAnalyzeDataSetOptions()
+        {
+            Console.WriteLine("\nChoose an option:");
+            Console.WriteLine("1. Find instances requiring additional annotation");
+            Console.WriteLine("2. Find instances with all disagreeing annotations");
+            Console.WriteLine("x. Exit\n");
+        }
+
+        private static void WriteAnnotationsConsistencyOptions()
+        {
+            Console.WriteLine("\nChoose an option:");
+            Console.WriteLine("1. Check annotation consistency for a single annotator");
+            Console.WriteLine("2. Check annotation consistency between annotators");
+            Console.WriteLine("3. Check metrics significance for a single annotator");
+            Console.WriteLine("4. Check metrics significance between annotators");
+            Console.WriteLine("x. Exit\n");
+        }
+
+        private static void ChosenOptionErrorMessage(string chosenOption)
+        {
+            Console.WriteLine("Option " + chosenOption + " not found. Choose again.\n");
         }
     }
 }
