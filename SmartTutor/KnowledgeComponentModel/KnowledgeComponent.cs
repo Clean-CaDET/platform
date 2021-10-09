ab6975ae46cf091fc4335ef7e5eceaa00a8b4525
n-dusan <nikolic.dusan.dey@gmail.com>
2021-10-09 11:54:59 +0200

    feat[st]: add initial knowledge component model

---
+++ SmartTutor/KnowledgeComponentModel/KnowledgeComponent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace SmartTutor.KnowledgeComponentModel
+{
+    public class KnowledgeComponent
+    {
+        public int Id { get; private set; }
+        
+        public string Name { get; private set; }
+
+        public List<KnowledgeComponent> KnowledgeSubcomponents { get; private set; }
+        
+    }
+}
